2b85069a8c29c66aac538eb40a33b0807d3a57ec
khk4912 <30457148+khk4912@users.noreply.github.com>
2021-05-05 02:41:19 +0900

    feat: Add missing parentheses

--- clicker.js
+++ clicker.js
@@ -1,7 +1,7 @@
 function addPonintCount() {
-    chrome.storage.local.get("channelPointCount", (res) => {
-        chrome.storage.local.set({ "channelPointCount": res.channelPointCount + 1 })
-    }
+  chrome.storage.local.get("channelPointCount", (res) => {
+    chrome.storage.local.set({ channelPointCount: res.channelPointCount + 1 })
+  })
 }
 function getButton() {
   return document.getElementsByClassName("tw-button tw-button--success")
