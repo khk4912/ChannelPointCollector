3fc2c82a5bcb0ca87f7e37ec2b44541bc21782c9
khk4912 <30457148+khk4912@users.noreply.github.com>
2023-11-28 02:49:39 +0900

    🔧 chore: Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,19 @@ const CopyPlugin = require('copy-webpack-plugin')
 const mode = process.env.NODE_ENV || 'development'
 
 const webpackConfig = {
+  mode: mode,
   entry: {
     content_script: path.resolve(__dirname, 'src/content_script.ts'),
-    background: path.resolve(__dirname, 'src/background.ts'),
+    popup: path.resolve(__dirname, 'src/popup.ts'),
   },
 
   output: {
     path: path.resolve(__dirname, './dist/js'),
     filename: '[name].js',
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js'],
   },
 
   module: {
@@ -28,6 +33,8 @@ const webpackConfig = {
       patterns: [{ from: 'public', to: '..' }],
     }),
   ],
+
+  devtool: false,
 }
 
 module.exports = webpackConfig
