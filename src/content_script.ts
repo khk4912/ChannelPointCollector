094ef5c1d193eea1735ef4ed0b820c5b513fc9cb
khk4912 <30457148+khk4912@users.noreply.github.com>
2023-11-28 04:00:49 +0900

    ✨ feat: Implement auto click

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,1 +1,19 @@
+import { addCountOf } from './utils/local_storage'
 
+const getID = (): string => new URL(location.href).pathname.split('/')[1]
+const ID = getID()
+
+function pointButtonWatcher(): void {
+  const bonusIcons = document.getElementsByClassName('claimable-bonus__icon')
+
+  if (bonusIcons.length === 0) {
+    return
+  }
+
+  const btn = bonusIcons[0] as HTMLElement
+  btn.click()
+
+  addCountOf(ID).then().catch(console.error)
+}
+
+setInterval(pointButtonWatcher, 1000)
