c4b11098844ee1d6b53b16daab8c05c450ded8da
khk4912 <30457148+khk4912@users.noreply.github.com>
2023-11-28 20:26:00 +0900

    🚑 fix: Revert 8216e1dd & Check ID every interval

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,12 +1,12 @@
 import { addCountOf } from './utils/local_storage'
 
 const getID = (): string => new URL(location.href).pathname.split('/')[1]
-const ID = getID()
 
 function pointButtonWatcher(): void {
   const bonusIcons = document.getElementsByClassName('claimable-bonus__icon')
+  const ID = getID()
 
-  if (bonusIcons.length === 0) {
+  if (bonusIcons.length === 0 || ID === '') {
     return
   }
 
@@ -16,6 +16,4 @@ function pointButtonWatcher(): void {
   addCountOf(ID).then().catch(console.error)
 }
 
-if (ID !== '') {
-  setInterval(pointButtonWatcher, 1000)
-}
+setInterval(pointButtonWatcher, 5000)
